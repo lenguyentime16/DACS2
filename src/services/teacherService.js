18f725e4bf961a70bae3eaeedf41b52f5e08e5e1
Loverboizz <binhnt.22git@vku.udn.vn>
2023-12-11 21:25:04 +0700

    #76

--- src/services/teacherService.js
+++ src/services/teacherService.js
@@ -167,19 +167,13 @@ let bulkCreateSchedule = (data) => {
                     raw: true
                 });
 
-                if (existing && existing.length >0 ) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;    
-                    })
-                }
 
                 let toCreate = _.differenceWith(schedule, existing, (a , b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 if (toCreate && toCreate.length > 0) {
-                    await db.Schedule.bulkCreate(schedule)
+                    await db.Schedule.bulkCreate(toCreate)
                 }
                
                 resolve({
@@ -206,14 +200,21 @@ let getScheduleByDate = (teacherId, date) => {
                     where: {
                         teacherId : teacherId,
                         date: date
-                    }
+                    },
+                    
+                    include: [
+
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = []
 
                 resolve({
                     errCode: 0,
-                    dataSchedule: dataSchedule
+                    data: dataSchedule
                 })
             }
 
