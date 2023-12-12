6d9bc5addfcb7be9585fd57a20b76dc9d9b1bd3a
Loverboizz <binhnt.22git@vku.udn.vn>
2023-12-13 01:01:12 +0700

    #78

--- src/migrations/migration-create-teacher-classroom-specialty.js
+++ src/migrations/migration-create-teacher-classroom-specialty.js
@@ -19,6 +19,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
---
+++ src/migrations/migration-teacher-infor.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teacher_infor', {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      teacherId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      priceId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },  
+      provinceId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      paymentId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      addressClassRoom: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nameClassRoom: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      note: {
+        type: Sequelize.STRING
+      },
+      count: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue : 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teacher_infor');
+  }
+};
